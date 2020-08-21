c43b9de7f17dc41c51534a36529caca7a77a82ef
Alexander Higgins <65186976+alhigginsucf@users.noreply.github.com>
2020-08-21 17:57:09 -0400

    updated index.html + main.js with functioning code

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,15 @@
-$.ajax({
-    url: "test.html",
-    context: document.body
-}).done(function() {
-    $(this).addClass("done");
-});
+function getData() {
+    var input = $("#searchtext").val()
+    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + input + "+&api_key=N5uojbvhaV0L8zHWaInhm2DCBqFJV66q&limit=30"); {
+        xhr.done(function(response) {
+
+            console.log("success got data", response);
+
+            var jiffs = response.data
+
+            for (i in jiffs) {
+                $('.inner').append("<img src='" + jiffs[i].images.original.url + "' style='height:350px; width:350px;' />")
+            }
+        });
+    }
+}
